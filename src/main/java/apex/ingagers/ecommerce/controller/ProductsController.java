a4e8ba89fe685c2cc2fa560a7415da2a21aad6fc
Roberto Lazo <roberto.lazo@alumnos.udg.mx>
2021-12-08 17:05:58 -0600

    Implemented version 1 of API REST

--- src/main/java/apex/ingagers/ecommerce/controller/ProductsController.java
+++ src/main/java/apex/ingagers/ecommerce/controller/ProductsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import apex.ingagers.ecommerce.model.Products;
@@ -21,6 +22,7 @@ import apex.ingagers.ecommerce.repository.SubCategoriesRepository;
 
 
 @RestController
+@RequestMapping("/api/v1")
 public class ProductsController {
 
     private final ProductsRepository productsRepository;
