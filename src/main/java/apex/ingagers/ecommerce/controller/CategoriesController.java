d2949227f15ba5c244ac954e773144d76483efa0
Otreb0r <42620567+Otreb0r@users.noreply.github.com>
2021-12-15 11:00:50 -0600

    [US-17] Add CRUD of Categories and SubCategories (#20)
    * Add Show all method for categories and subcategories
    
    * Update the atributes of sucbategorie and categorie
    
    * Remove imports
    
    * Add endpoint for swagger

---
+++ src/main/java/apex/ingagers/ecommerce/controller/CategoriesController.java
@@ -0,0 +1,34 @@
+package apex.ingagers.ecommerce.controller;
+
+import java.util.Optional;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import apex.ingagers.ecommerce.model.Categories;
+import apex.ingagers.ecommerce.repository.CategoriesRepository;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CategoriesController {
+    
+    private final CategoriesRepository categoriesRepository;
+
+    CategoriesController(CategoriesRepository categoriesRepository){
+        this.categoriesRepository = categoriesRepository;
+    }
+
+    @GetMapping("/categories")
+    public List<Categories> getAllCategories(){
+        return categoriesRepository.findAll();
+    }
+
+    @GetMapping("/categories/{id}")
+    public Optional<Categories> getCategoriesById(@PathVariable("id") Integer id){
+        return categoriesRepository.findCategoriesById(id);
+    }
+
+}
--- src/main/java/apex/ingagers/ecommerce/repository/SubCategoriesRepository.java
+++ src/main/java/apex/ingagers/ecommerce/repository/SubCategoriesRepository.java
@@ -2,9 +2,19 @@ package apex.ingagers.ecommerce.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+import java.util.List;
+import java.util.Optional;
 
 import apex.ingagers.ecommerce.model.SubCategories;
 
 public interface SubCategoriesRepository extends JpaRepository<SubCategories, Integer> {
     SubCategories findByName(String name);
+    @Query(value = "SELECT * FROM sub_categories WHERE  is_active = 1", nativeQuery = true)
+    List<SubCategories> findAllCategories();
+
+    @Query(value = "SELECT * FROM sub_categories WHERE  is_active = 1 and id=?1", nativeQuery = true)
+    Optional<SubCategories> findSubCategoriesById(Integer id);
 }
