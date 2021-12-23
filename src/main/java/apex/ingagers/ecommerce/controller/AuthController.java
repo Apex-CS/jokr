a2c038373252c936f1f3d83d3ef0988cea5207b7
Roberto Lazo <roberto.lazo@alumnos.udg.mx>
2021-12-23 13:28:04 -0600

    Add basic structure og login

---
+++ src/main/java/apex/ingagers/ecommerce/controller/AuthController.java
@@ -0,0 +1,29 @@
+package apex.ingagers.ecommerce.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import apex.ingagers.ecommerce.model.Users;
+import apex.ingagers.ecommerce.repository.UserRepository;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AuthController {
+
+    private final UserRepository userRepository;
+  
+    AuthController(UserRepository userRepository) {
+      this.userRepository = userRepository;
+    }
+
+    @PostMapping("/Auth") // Map ONLY POST Requests
+    public boolean  login (@RequestBody Users user) {
+        List <Users> lista = userRepository.VerifyCredentials(user.getEmail(), user.getPassword());
+        return !lista.isEmpty();
+    }
+    
+}
--- src/main/java/apex/ingagers/ecommerce/repository/UserRepository.java
+++ src/main/java/apex/ingagers/ecommerce/repository/UserRepository.java
@@ -15,6 +15,13 @@ public interface UserRepository extends JpaRepository<Users, Integer> {
    @Query(value = "SELECT * FROM users WHERE  is_active = 1 and id=?1", nativeQuery = true)
    Optional<Users> findUserById(Integer id);
 
+<<<<<<< HEAD
    Optional<Users> findByName(String name);
+=======
+   @Query(value = "SELECT * FROM users WHERE  is_active = 1 and email=?1 and password=?2", nativeQuery = true)
+   List<Users> VerifyCredentials(String email, String password );
+
+   Users findByName(String name);
+>>>>>>> 12bd6f0 (Add basic structure og login)
 
 }
