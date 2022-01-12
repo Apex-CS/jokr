19894917a778497dc21338934fa17255a7bc4161
David Hernandez Aznar <dae.aznar@gmail.com>
2022-01-12 12:07:27 -0600

    Fix minor typos

--- src/main/java/apex/ingagers/ecommerce/repository/OrderStatusesRepository.java
+++ src/main/java/apex/ingagers/ecommerce/repository/OrderStatusesRepository.java
@@ -3,5 +3,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import apex.ingagers.ecommerce.model.OrderStatuses;
 
 public interface OrderStatusesRepository extends JpaRepository<OrderStatuses, Integer> {
-   OrderStatuses findByStatusName(String status_name);
 }
