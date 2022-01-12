0217f1b12670df21534b4d0de0c30b5d3c6ff3e2
David Hernandez Aznar <dae.aznar@gmail.com>
2022-01-12 13:34:06 -0600

    Organize imports

--- src/main/java/apex/ingagers/ecommerce/model/Addresses.java
+++ src/main/java/apex/ingagers/ecommerce/model/Addresses.java
@@ -1,9 +1,15 @@
 package apex.ingagers.ecommerce.model;
 
 import java.sql.Timestamp;
-import java.util.Optional;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
