db0985137d57106f96e18711c4541661e11acade
David Hernandez Aznar <dae.aznar@gmail.com>
2022-01-20 00:47:40 -0600

    Update OrderStatuses Repository with new functionality

--- src/main/java/apex/ingagers/ecommerce/repository/OrderStatusesRepository.java
+++ src/main/java/apex/ingagers/ecommerce/repository/OrderStatusesRepository.java
@@ -1,7 +1,14 @@
 package apex.ingagers.ecommerce.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 import apex.ingagers.ecommerce.model.OrderStatuses;
 
 public interface OrderStatusesRepository extends JpaRepository<OrderStatuses, Integer> {
     OrderStatuses findByStatusname(String status_name);
+
+    @Query(value = "SELECT * FROM order_statuses WHERE  is_active = 1 and id=?1", nativeQuery = true)
+    List<OrderStatuses> findStatusById(Integer id);
 }
